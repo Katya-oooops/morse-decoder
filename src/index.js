36164ac01f56560ecabf950cb06e5c39885130f1
Katya-oooops <54660197+Katya-oooops@users.noreply.github.com>
2019-10-05 23:20:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+      var ar = expr.match(/(.{2})/g);
+
+    for (let i=0; i < ar.length; i++){
+      if (ar[i] == '10'){
+        ar[i] = '.';
+      } else if (ar[i] == '11'){
+        ar[i] = '-';
+      } else if (ar[i] == '00'){
+        ar[i]='';
+      }
+    }
+    
+    let arr = [];
+    
+      for ( let m=0; m < ar.length; m+4){
+        let val=ar[m]+ar[m+1]+ar[m+2]+ar[m+3]+ar[m+4];
+        arr.push(val);
+      }
+
+    var sum=0;
+
+      for (let n=0; n < arr.length ; n++){
+          let test = arr[n] 
+          
+          sum+=MORSE_TABLE[test];
+      }
+
+     return sum;
 }
 
 module.exports = {
