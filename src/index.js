9845352c6d4e89c876d604147880e8e14ea467b6
Katya-oooops <courage.wisdom.force@gmail.com>
2019-10-06 12:23:10 +0300

    fix: second

--- src/index.js
+++ src/index.js
@@ -39,34 +39,18 @@ const MORSE_TABLE = {
 
 function decode(expr) {
    
-    var ar = expr.match(/(.{2})/g);
+  var arr = expr.replace(/11/g, '-').replace(/10/g, '.').replace(/00/g, ',');
+  let i=0;
+  var myString='';
+  while (i < arr.length){
+    var test = arr.slice(i, i+5);
+    var testB = test.replace(/,/g, '');
+    myString+=MORSE_TABLE[testB];
+    i=i+5;
+  }  
 
-    for (let i=0; i < ar.length; i++){
-      if (ar[i] == '10'){
-        ar[i] = '.';
-      } else if (ar[i] == '11'){
-        ar[i] = '-';
-      } else if (ar[i] == '00'){
-        ar[i]='';
-      }
-    }
-    
-    let arr = [];
-    
-      for ( let m=0; m < ar.length; m+4){
-        let val=ar[m]+ar[m+1]+ar[m+2]+ar[m+3]+ar[m+4];
-        arr.push(val);
-      }
-
-    var sum=0;
-
-      for (let n=0; n < arr.length ; n++){
-          let test = arr[n] 
-          
-          sum+=MORSE_TABLE[test];
-      }
-
-     return sum;
+  return myString;
+  
 }
 
 module.exports = {
